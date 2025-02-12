537169eea195a48a8473c7dac198c427a0d52d76
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-02-12 16:34:50 +0100

    connexion database

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 $config = require('config.php'); 
 
 try {
-    $pdo = new PDO("mysql:host=" . $config['db_host'] . ";dbname=" . $config['db_name'] . ";charset=utf8mb4", $config['db_user'], $config['db_pass']);  
+    $pdo = new PDO("mysql:host=" . $config['db_host'] . ";dbname=" . $config['db_name'] . ";charset=utf8mb4", $config['db_user'], $config['db_pass'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
