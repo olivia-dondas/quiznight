2a0237a33dcc24777f73b57af01a2ebb97176cfb
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-02-13 14:38:55 +0100

    test  register.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$config = require('./config/config.php'); 
+$config = require('config/config.php'); 
 
 // Connexion à la base de données avec PDO
 try {
