5720c1e8cce889f1f1256754dd415b5e857bdc17
Olivia Dondas <olivia.dondas@laplateforme.io>
2025-02-13 22:08:50 +0100

    correction erreur config.php

--- public/quiz.php
+++ public/quiz.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $config = require('config/config.php'); 
-
+var_dump(file_exists('config/config.php'));  // Affiche true si le fichier existe
 // Connexion à la base de données avec PDO
 try {
     $pdo = new PDO(
